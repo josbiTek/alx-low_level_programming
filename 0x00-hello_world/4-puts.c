e80a0e9fbbdc3078c22f230e49bbcb77c85bbf66
Joseph <folorunsojoseph8512@gmail.com>
2022-09-08 16:46:23 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,11 +4,11 @@
  * main- Serves as the entry point for the program
  *
  * Return: By default returns zero if no errors
- * otherwise returns non-zero value.
+ * Otherwise returns non-zero value.
  */
 
 int main(void)
 {
-	puts("\	"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return	(0);
 }
