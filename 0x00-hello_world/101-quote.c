4f904fd3313f515fe3d51b83dfab0b3fc207ccbf
Joseph <folorunsojoseph8512@gmail.com>
2022-09-08 17:21:31 -0700

    Unix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,11 @@
 #include <unistd.h>
 
 /**
- * main-program entry point.
+ * main - prints exactly "and that piece of art is useful"
  *
- * Return: 1 on success.
+ * - Dora Korpar, 2015-10-19,
+ * followed by a new line, to the standard error.
+ * Return: Always 0 (success)
  */
 
 int main(void)
