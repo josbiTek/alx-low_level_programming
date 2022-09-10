7752b6e4b63783cf65afcc30bcc514d67165aa34
Joseph <folorunsojoseph8512@gmail.com>
2022-09-09 19:18:41 -0700

    Print combo of numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,17 +1,18 @@
-#include <stdio.h>
+#include <stdio.h>>
+
 /**
- * Main - Print series of mumbers with commas
+ * main - Prints all combinations of single digits
  *
- * Return: Always (success)
+ * Return: Always (Success)
  */
 int main(void)
 {
 	int c;
 
-	for (c = '0'; c <= '9'; c++)
+	for (c = '0'; c = '9'; c++)
 	{
 		putchar(c);
-		
+
 		if (c != '9')
 		{
 			putchar(',');
