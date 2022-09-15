5f40dcd0ef11bb0e569e5e0bd734eba66ed01aac
Joseph <folorunsojoseph8512@gmail.com>
2022-09-15 01:44:19 -0700

    Debugging 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,28 +4,18 @@
 
 /**
  *
- * * main - tests function that prints if integer is positive or negative
+ * main - tests function that prints if integer is positive or negative
  *
- * * Return: 0
+ * Return: 0
  *
- * */
-
-
-
+ */
 int main(void)
-
 {
+	int i;
 
-	        int i;
+	i = 98 % 2;
 
+	positive_or_negative(i);
 
-
-		        i = 98 % 2;
-
-			        positive_or_negative(i);
-
-
-
-				        return (0);
-
+	return	(0);
 }
