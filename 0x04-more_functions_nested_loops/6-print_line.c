f72f01269d76f9e1289a1150a7fa630d3c8f2293
Joseph <folorunsojoseph8512@gmail.com>
2022-09-16 15:04:25 -0700

    print straight line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,19 +2,20 @@
 
 /**
  * print_line - print a straight line
+ *
  * @n: is the number of times the _ character
  * should be printed
  */
 
 void print_line(int n)
 {
-	int lnChr;
+	int ln;
 
 	if (n <= 0)
 		_putchar('\n');
 	else
 	{
-		for (lnchr = 1; lnChr <= n; ++lnChr)
+		for (lnc = 1; ln <= n; ++ln)
 			_putchar('_');
 		_putchar('\n');
 	}
